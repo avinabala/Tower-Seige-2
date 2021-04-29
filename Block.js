4f007f67c3a8ce24fe7894c4b371c8a5f3ea65f5
avinabala <78329570+avinabala@users.noreply.github.com>
2021-04-28 22:40:17 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,31 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("block.png")
+      this.visibilty = 255
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+
+      if(this.body.speed<3){
+        super.display()
+      }
+      else{
+        World.remove(world,this.body)
+        push ()
+        this.visiblity = this.visiblity-5
+        tint (255,this.visiblity)
+        image(this.image,this.body.position.x,this.body.position.y,50,50)
+        pop ()
+      }
+    }
+}
