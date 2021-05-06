128ca864823a9c039a63f02b6060b7cf2940a60b
avinabala <78329570+avinabala@users.noreply.github.com>
2021-05-05 22:08:06 -0400

    Add files via upload

--- Block.js
+++ Block.js
@@ -14,18 +14,20 @@ class Block{
     }
    
     display(){
-      console.log(this.body.speed);
       var pos= this.body.position;
       if(this.body.speed <3){
-        imageMode(CENTER);
-        image(this.image,pos.x,pos.y,this.width, this.height);
-      }
-    else{
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+      }else{
       World.remove(world, this.body);
       push();
       this.visiblity = this.visiblity - 5;
       tint(255,this.visiblity);
-      image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+      image(this.image, 0, 0,this.width, this.height);
       pop();
     }
   }
